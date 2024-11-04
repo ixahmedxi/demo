f1ccc9b42c54686f523a92e96869920fae970fab
Ahmed Elsakaan <ahmedtarek3214@gmail.com>
2024-11-04 17:37:00 +0000

    diabolical codegen

--- apps/web2/src/App.tsx
+++ apps/web2/src/App.tsx
@@ -1,7 +1,12 @@
 import { trpc } from './lib/trpc'
 
 export function App() {
-  const { data } = trpc.router19.greeting.useQuery({ who: 'world' })
+  const { data } =
+    trpc.router0.quantum.multiverse.infiniteRecursion.recurse.useQuery({
+      depth: 1,
+      data: { hello: 'world' },
+      callback: () => Promise.resolve(),
+    })
 
   return <h1>{data ?? 'Loading...'}</h1>
 }
