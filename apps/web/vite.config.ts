04f73a9cdeedacef63c9662df9acb771bd44d82d
Ahmed Elsakaan <ahmedtarek3214@gmail.com>
2024-11-04 17:49:55 +0000

    i hate this one

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,6 +1,11 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['@org/api', '@org/utils', '@org/utils/base-url'],
+    },
+  },
 })
